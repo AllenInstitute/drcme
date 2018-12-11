140e8e3d08e4392ede8c38ed25769bcaf0d8c1d5
Nathan Gouwens <nathang@alleninstitute.org>
2018-12-11 13:55:34 -0800

    RF prediction bugfix and customization

--- drcme/bin/run_rf_prediction.py
+++ drcme/bin/run_rf_prediction.py
@@ -12,12 +12,15 @@ class RfPredictionParameters(ags.ArgSchema):
     label_key = ags.fields.String()
     morph_file = ags.fields.InputFile(default=None, allow_none=True)
     output_file = ags.fields.OutputFile()
+    ref_id_file = ags.fields.InputFile(default=None, allow_none=True)
+    pred_id_file = ags.fields.InputFile(default=None, allow_none=True)
     n_trees = ags.fields.Integer(default=500)
     class_weight = ags.fields.String(default=None, allow_none=True)
 
 
 def main(reference_ephys_file, prediction_ephys_file, reference_label_file,
-         label_key, morph_file, output_file, n_trees, **kwargs):
+         label_key, morph_file, output_file, ref_id_file, pred_id_file,
+         n_trees, class_weight, **kwargs):
     ephys_ref = pd.read_csv(reference_ephys_file, index_col=0)
     ephys_pred = pd.read_csv(prediction_ephys_file, index_col=0)
 
@@ -30,18 +33,27 @@ def main(reference_ephys_file, prediction_ephys_file, reference_label_file,
     else:
         morph_df = pd.read_csv(morph_file, index_col=0)
         ref_df, test_df = construct_datasets(ephys_ref, ephys_pred, morph_df)
+
+    if ref_id_file is not None:
+        ref_ids = np.loadtxt(ref_id_file)
+        ref_df = ref_df.loc[ref_ids, :]
+    if pred_id_file is not None:
+        pred_ids = np.loadtxt(pred_id_file)
+        test_df = test_df.loc[pred_ids, :]
+
     labels = ref_label_df.loc[ref_df.index, label_key].values
 
-    # drop values that don't have labels
+    # drop reference values that don't have labels
     nan_mask = ~pd.Series(labels).isnull().values
     labels = labels[nan_mask]
     ref_df = ref_df.loc[nan_mask, :]
 
-    pred_labels = pred.rf_predict(ref_df, labels, ephys_pred, n_trees=n_trees)
+    pred_labels = pred.rf_predict(ref_df, labels, test_df,
+                                  n_trees=n_trees, class_weight=class_weight)
 
 
     logging.debug("Saving results")
-    pd.DataFrame(pred_labels, index=ephys_pred.index.values).to_csv(output_file)
+    pd.DataFrame(pred_labels, index=test_df.index.values).to_csv(output_file)
 
 
 def construct_datasets(ephys_ref, ephys_pred, morph_df):
